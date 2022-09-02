207b48f537229658761e4ae87b57dca837c04e75
qq8721443 <qq8721443@naver.com>
2022-09-02 22:40:17 +0900

    refactor: 불필요 상수 변수 제거

--- src/constants/query-constants.js
+++ src/constants/query-constants.js
@@ -3,4 +3,3 @@ export const QUERY_ADD_BUTTON = ".add-button";
 export const QUERY_REMOVE_BUTTON = ".remove-button";
 export const QUERY_NAME_TITLE = "[name=title]";
 export const QUERY_NAME_CONTENT = "[name=content]";
-export const UNTITLED = "untitled";
