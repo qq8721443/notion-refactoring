ad796ab47b2a2e3558354c8410dc5e963a7a0e03
qq8721443 <qq8721443@naver.com>
2022-09-02 22:08:41 +0900

    feat: 메인 페이지 추가

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -15,6 +15,13 @@ export default function HomePage({ $target, initialState }) {
   };
 
   this.render = () => {
-    $homePage.innerHTML = "homepage!";
+    $homePage.innerHTML = `
+      <div class="rich-editor-container">
+        <div class="rich-editor">
+          <div style="font-size: 40px; font-weight: bold; padding-top: 20px">Vanilla JS 노션 클로닝 프로젝트</div>
+          <div style="margin-top: 48px;">자바스크립트를 사용해 만든 노션 클로닝 페이지입니다.</div>
+        </div>
+      </div>
+    `;
   };
 }
