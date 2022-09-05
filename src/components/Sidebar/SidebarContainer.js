07d27da00489fdda874ee6d854720a701a90d69e
qq8721443 <qq8721443@naver.com>
2022-09-06 01:01:48 +0900

    feat: 사이드바 크기 조절 기능 1차 완성

--- src/components/Sidebar/SidebarContainer.js
+++ src/components/Sidebar/SidebarContainer.js
@@ -1,6 +1,7 @@
 import DocumentsList from "./DocumentsList.js";
 import NewButton from "./NewButton.js";
 import SidebarTitle from "./SidebarTitle.js";
+import throttle from "../../utils/throttle.js";
 
 export default function SidebarContainer({
   $target,
@@ -15,6 +16,12 @@ export default function SidebarContainer({
 }) {
   const $container = document.createElement("div");
   $container.classList.add("sidebar-container");
+
+  const $side = document.createElement("div");
+  $side.classList.add("side");
+  $side.setAttribute("draggable", true);
+  $container.appendChild($side);
+
   $target.appendChild($container);
 
   this.state = initialState;
@@ -50,4 +57,22 @@ export default function SidebarContainer({
   });
 
   this.render = () => {};
+
+  const handleDragStart = (e) => {
+    // e.preventDefault();
+    const img = new Image();
+    img.src =
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/HD_transparent_picture.png/1600px-HD_transparent_picture.png?20200606142532";
+    e.dataTransfer.setDragImage(img, 0, 0);
+  };
+
+  const handleDrag = (e) => {
+    console.log($container.getBoundingClientRect().width);
+    $container.style.width = `${
+      $container.getBoundingClientRect().width + e.offsetX
+    }px`;
+  };
+
+  $side.addEventListener("dragstart", handleDragStart);
+  window.addEventListener("drag", throttle(handleDrag));
 }
